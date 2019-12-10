1f87d565a3ecb1c39712747dc37c40ce8ed36dd8
DarneaPinkett <56055954+DarneaPinkett@users.noreply.github.com>
2019-12-10 10:12:35 -0500

    P1

--- Article/Article.js
+++ Article/Article.js
@@ -112,3 +112,12 @@ const data = [
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
 
 */
+const article = document.querySelector('.article');
+
+// createElements
+function createArticle(title, date, firstParagraph, secondParagraph, thirdParagraph) {
+  const articleTitle = document.createElement('h2');
+  const dateArticle = document.createElement('p');
+  const paragraph = document.createElement('p');
+  const button = document.createElement('span');
+}
